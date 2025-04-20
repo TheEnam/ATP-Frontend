dafc807b3d5274f2849b0ee593360b9c56594ec8
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2025-04-20 19:37:53 +0000

    update verifyEmail link

--- src/api/auth/verifyEmail.js
+++ src/api/auth/verifyEmail.js
@@ -3,7 +3,7 @@ import axios from "../axiosInstance";
 export const verifyEmail = async (code) => {
   try {
     console.log("Verifying email with token:", code);
-    const response = await axios.post("http://localhost:3000/auth/email/verify", { code });
+    const response = await axios.post("/auth/email/verify", { code });
     return response.data;
   } catch (error) {
     console.error("Verification API error:", error.response?.data || error.message);

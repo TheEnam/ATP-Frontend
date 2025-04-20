bc25eeb0c7759b9fcb9909aaf1f919468b91ac9f
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2025-04-20 20:04:28 +0000

    baseurl

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "atp-apis.onrender.com",
+  baseURL: "https://atp-apis.onrender.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",

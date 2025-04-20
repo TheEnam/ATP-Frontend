a43075a4014c1bbdec346b0af312ff171d32dfa1
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2025-04-20 22:28:06 +0000

    problem: Fix axiosInstance.js to use the correct base URL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: "https://atp-apis.onrender.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",

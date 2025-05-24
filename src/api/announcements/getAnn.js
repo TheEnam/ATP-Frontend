baeed75482d908d4997ffafb1a99c650cb351072
Akua-Serwaa-Nkrumah <90011656+Akua-Serwaa-Nkrumah@users.noreply.github.com>
2025-05-24 11:57:30 +0000

    Update getAnn.js

--- src/api/announcements/getAnn.js
+++ src/api/announcements/getAnn.js
@@ -3,9 +3,9 @@ import axios from "../axiosInstance";
 export const getAnn = async (filters) => {
   const params = {};
 
-  if (filters.startDate) query.append("startDate", filters.startDate);
-  if (filters.endDate) query.append("endDate", filters.endDate);
-  if (filters.typeOfAnnouncement) query.append("typeOfAnnouncement", filters.typeOfAnnouncement);
+  if (filters.startDate) params.startDate = filters.startDate;
+  if (filters.endDate) params.endDate = filters.endDate;
+  if (filters.typeOfAnnouncement) params.typeOfAnnouncement = filters.typeOfAnnouncement;
 
   const response = await axios.get(`/announcement`,{params});
   return response.data;

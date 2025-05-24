dd503fa89c8f0fa274c5059a0e25c3197570e6b5
Akua-Serwaa-Nkrumah <90011656+Akua-Serwaa-Nkrumah@users.noreply.github.com>
2025-05-24 11:34:31 +0000

    Create getAnnbyId.js

---
+++ src/api/announcements/getAnnbyId.js
@@ -0,0 +1,16 @@
+import axios from '../axiosInstance';
+
+export const getAnnbyId = async (id) => {
+    const token = localStorage.getItem("token") || sessionStorage.getItem("token");
+    if (!token) {
+        throw new Error("No token found. Please log in.");
+    }
+
+   
+    const response = await axios.get(`/announcement/${id}`);
+    return response.data;
+    // } catch (error) {
+    //     console.error("Error fetching announcement by ID:", error);
+    //     throw error;
+    // }
+}

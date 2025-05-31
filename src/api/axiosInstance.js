ed522cae497b3fcd297d76419ea74b2582ec0551
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2025-05-31 14:46:18 +0000

    logo error

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://localhost:3000",
+  // baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
   baseURL: process.env.NODE_ENV === "development"
     ? ""
     : process.env.REACT_APP_API_BASE_URL,
@@ -10,15 +10,15 @@ const axiosInstance = axios.create({
   },
 });
 
-axiosInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token") || sessionStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    else {
-      delete config.headers.Authorization;
-    }
-    return config;
-  });
+// axiosInstance.interceptors.request.use((config) => {
+//     const token = localStorage.getItem("token") || sessionStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     else {
+//       delete config.headers.Authorization;
+//     }
+//     return config;
+//   });
   
 export default axiosInstance;

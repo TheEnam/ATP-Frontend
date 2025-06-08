3f331ec5458f815f0e92f222d6560f4723740144
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2025-06-08 12:27:24 +0000

    fix: Update axios instance to handle errors more gracefully

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: 'https://atp-apis.onrender.com',
   // baseURL: process.env.NODE_ENV === "development"
   //   ? ""
   //   : process.env.REACT_APP_API_BASE_URL,

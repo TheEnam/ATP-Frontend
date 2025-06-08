9682654e57f6c93689800a5e02d5880ccf941a7f
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2025-06-08 12:09:49 +0000

    axiosInstance.js: Update axios instance to include custom headers

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
-  baseURL: process.env.NODE_ENV === "development"
-    ? ""
-    : process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  // baseURL: process.env.NODE_ENV === "development"
+  //   ? ""
+  //   : process.env.REACT_APP_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
